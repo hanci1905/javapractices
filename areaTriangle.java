d5e60c242d6adc62ffab885d37704a80c36d3d0c
hanci1905 <83466117+hanci1905@users.noreply.github.com>
2021-11-07 22:35:41 +0300

    Create areaTriangle.java

---
+++ areaTriangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class hipotenus {
+    public static void main( String [] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter first side of trianlgle");
+        int first=input.nextInt();
+        System.out.println("enter second side of trianlgle");
+        int second=input.nextInt();
+        System.out.println("enter hipotenus of trianlgle");
+        int hipotenus=input.nextInt();
+        double sphere=(hipotenus+second+first)/2.0;
+        double area;
+        double realArea;
+        area=sphere*(sphere-first)*(sphere-second)*(sphere-hipotenus);
+        realArea=Math.sqrt(area);
+        System.out.println("area of the triangle is " + realArea);
+
+    }
+
+    }
