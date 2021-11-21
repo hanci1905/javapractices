f155ff98006705a91ab3d77c464d5ed33b114620
hanci1905 <83466117+hanci1905@users.noreply.github.com>
2021-11-21 17:15:18 +0300

    Create event.java

---
+++ event.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int degree;
+
+        System.out.println("enter degree ");
+        degree=input.nextInt();
+        if (degree < 5 ) {
+            System.out.println("kayak yapabilirsiniz ");
+        }
+        else if (degree < 15 && degree >= 5) {
+            System.out.println("sinemaya gidebilirsiniz ");
+
+        }
+        else if (degree < 25 && degree >= 15) {
+           System.out.println("piknige gidebilirsiniz ");
+        }
+        else if (degree > 25) {
+          System.out.println("yuzmeye gidebilirsiniz ");
+
+        }
+
+    }
+}
