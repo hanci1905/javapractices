6d2b0f747977b0ee965baaea2cd285a255b13d57
hanci1905 <83466117+hanci1905@users.noreply.github.com>
2021-11-22 22:42:34 +0300

    Update password.java

--- password.java
+++ password.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class password {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
