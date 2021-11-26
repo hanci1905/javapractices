1143cb43f9f7476cf9308b99a62420cc80bd1a27
hanci1905 <kaanfre@gmail.com>
2021-11-27 00:24:37 +0300

    prime numbers till 100

---
+++ Prime.java
@@ -0,0 +1,18 @@
+package com.company.javapractices;
+
+public class Prime {
+    public static void main(String[] args) {
+
+    int counter=0;
+        for (int i = 2; i < 100; i++) {
+            for (int j = 2; j < i; j++) {
+                if(i%j==0){
+                    counter++;}
+            }
+            if(counter==0){
+                System.out.print(" " + i);
+            }
+            counter=0;
+    }
+
+}}
